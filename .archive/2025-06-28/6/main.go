3a427caec809d9ea36e335ca08dbdeb2ec356f5b
Philipp Maier <25420070+philmph@users.noreply.github.com>
2025-07-02 21:57:02 +0200

    Building a interface from memory

---
+++ .archive/2025-06-28/6/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Walker interface {
+	Walk(int) error
+}
+
+type Human struct {
+	km int
+}
+
+func (h *Human) Walk(distance int) error {
+	h.km += distance
+
+	return nil
+}
+
+func main() {
+	h := &Human{
+		km: 0,
+	}
+
+	err := h.Walk(10)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Distance:", h.km)
+}
