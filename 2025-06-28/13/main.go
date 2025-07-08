32212f9d377cb742f3b4cf2c35d6fa7f83008bb5
Philipp Maier <25420070+philmph@users.noreply.github.com>
2025-07-08 16:33:03 +0200

    Experiment with channels

---
+++ 2025-06-28/13/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	msgch := make(chan string, 128)
+
+	for i := 0; i < 10; i++ {
+		msgch <- ("Hello " + strconv.Itoa(i))
+	}
+
+	close(msgch)
+	for msg := range msgch {
+		fmt.Println(msg)
+	}
+
+}
